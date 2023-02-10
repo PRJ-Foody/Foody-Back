54fc51993351fe030191c589a9525cc2f88c653a
maniac-O <cj45645@naver.com>
2023-02-10 21:11:56 +0900

    [test] : Test Commit

---
+++ src/main/java/com/prjfoody/foody/modules/sdsJajonsim.java
@@ -0,0 +1,10 @@
+package com.prjfoody.foody.modules;
+
+public class sdsJajonsim {
+
+    sdsJajonsim(){
+        while(true){
+            System.out.println("sdsJajonsim.sdsJajongam !!!!!");
+        }
+    }
+}
